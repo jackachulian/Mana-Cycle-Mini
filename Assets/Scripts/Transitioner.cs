82efa44e2fe3f054504542a07cafc1b108cc7366
three-halves <threehalves1@gmail.com>
2023-09-01 16:39:54 -0500

    transition work

---
+++ Assets/Scripts/Transitioner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor.Animations;
+
+public class Transitioner : MonoBehaviour
+{
+
+    [SerializeField] Animator animator;
+    private string nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // called by other objects to switch scene smoothly
+    public void TransitionToScene(string s)
+    {
+        nextScene = s;
+        animator.SetTrigger("Wipe");
+    }
+
+    // called by an event in the transition animation
+    private void SwitchScene()
+    {
+        // nextScene set by TransitionToScene function when its called
+        Debug.Log("Loaded Scene " + nextScene);
+        animator.ResetTrigger("Wipe");
+        SceneManager.LoadScene(nextScene);
+        
+    }
+}
